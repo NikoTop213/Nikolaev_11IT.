a2d990ab2fb314fd287a3d8af5269886c52cd35c
NikoTop213 <egornikolaev906@gmail.com>
2026-04-20 19:20:27 +0300

    Add files via upload

---
+++ 13.2.py
@@ -0,0 +1,9 @@
+n = int(input("Введите количество строк: "))
+
+all_simvoli = []
+
+for _ in range(n):
+    text_line = input("Введите строку: ")
+    all_simvoli.extend(list(text_line))
+
+print(f"Список всех символов: {all_simvoli}")
